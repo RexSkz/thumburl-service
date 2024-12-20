eb69a362f210bc5d4311755c402d191acfac0048
Rex Zeng <rex@rexskz.info>
2024-12-21 00:26:06 +0800

    feat: mock chrome user-agent

--- internal/service/metaservice/metaservice.go
+++ internal/service/metaservice/metaservice.go
@@ -7,7 +7,14 @@ import (
 )
 
 func GetMeta(url string) (*htmlmeta.HTMLMeta, error) {
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
